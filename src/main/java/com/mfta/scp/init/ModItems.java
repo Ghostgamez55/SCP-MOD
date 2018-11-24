51183188b5d2397fbba74530a92ebf9a8b1e3f3b
George Kazanjian <soggymustacheofficialmods@gmail.com>
2018-11-24 16:22:00 -0500

    Add SCP armor and fix dimensions
    Added in the SCP armor as well as fixing the dimensions double registering

--- src/main/java/com/mfta/scp/init/ModItems.java
+++ src/main/java/com/mfta/scp/init/ModItems.java
@@ -1,31 +1,41 @@
 package com.mfta.scp.init;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mfta.scp.Reference;
+import com.mfta.scp.init.items.ItemSpecialArmor;
 import com.mfta.scp.init.items.ItemFlareGun;
 import com.mfta.scp.init.items.ItemHungryBag;
 import com.mfta.scp.init.items.ItemSCP;
 import com.mfta.scp.init.items.food.FoodBase;
 import com.mfta.scp.init.items.food.FoodEfectBase;
 import com.mfta.scp.tabs.SCPTabs;
+
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class ModItems {
 	
 	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final ArmorMaterial SCP = EnumHelper.addArmorMaterial("scp", Reference.MOD_ID + ":scp", 33, new int[]{4, 7, 9, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F);
+	
+	public static final int SCP_ARMOR = 0;
 	
 	public static final Item HUNGRY_BAG = setUpItem(new ItemHungryBag(), "hungry_bag");
 	//food
@@ -37,6 +47,12 @@ public class ModItems {
 	public static final Item INFECTED_PORKCHOP = setUpItem(new FoodEfectBase(0, 0f, false, new PotionEffect(MobEffects.HUNGER, (60 * 20), 2, false, true)), "infected_porkchop");
 	public static final Item FLARE_AMMO = setUpItem(new ItemSCP(), "flare_ammo");
 	public static final Item FLARE_GUN = setUpItem(new ItemFlareGun(), "flare_gun");
+	
+	public static final Item SCP_ARMOR_HEAD = setUpItem(new ItemSpecialArmor(SCP_ARMOR, SCP, 0, EntityEquipmentSlot.HEAD), "scp_armor_head");
+	public static final Item SCP_ARMOR_CHEST = setUpItem(new ItemSpecialArmor(SCP_ARMOR, SCP, 0, EntityEquipmentSlot.CHEST), "scp_armor_chest");
+	//Seems like there is no model for these?
+//	public static final Item SCP_ARMOR_LEGS = setUpItem(new ItemSpecialArmor(SCP_ARMOR, SCP, 1, EntityEquipmentSlot.LEGS), "scp_armor_legs");
+//	public static final Item SCP_ARMOR_BOOTS = setUpItem(new ItemSpecialArmor(SCP_ARMOR, SCP, 0, EntityEquipmentSlot.FEET), "scp_armor_boots");
 	
 	
 	@SubscribeEvent
--- src/main/java/com/mfta/scp/proxy/ClientProxy.java
+++ src/main/java/com/mfta/scp/proxy/ClientProxy.java
@@ -1,14 +1,20 @@
 package com.mfta.scp.proxy;
 
+import com.mfta.scp.client.model.armor.ModelSCPArmor;
 import com.mfta.scp.client.render.RenderBigFoot;
 import com.mfta.scp.client.render.RenderBilly;
 import com.mfta.scp.client.render.RenderFlareProjectile;
+import com.mfta.scp.init.ModItems;
 import com.mfta.scp.init.entity.EntityBigFoot;
 import com.mfta.scp.init.entity.EntityBilly;
 import com.mfta.scp.init.entity.projectiles.EntityFlare;
+
+import net.minecraft.client.model.ModelBiped;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy {
+	
+	private static final ModelSCPArmor SCP_ARMOR = new ModelSCPArmor();
 	
 	@Override
 	public void registerRenders() {
@@ -16,4 +22,17 @@ public class ClientProxy extends CommonProxy {
 		RenderingRegistry.registerEntityRenderingHandler(EntityBilly.class, new RenderBilly.RenderFactory());
 		RenderingRegistry.registerEntityRenderingHandler(EntityFlare.class, RenderFlareProjectile::new);
 	}
+
+	@Override
+	public ModelBiped getArmorModel(int ID, int type) {
+		if(ID == ModItems.SCP_ARMOR) {
+			switch(type) {
+				case 0:
+					return SCP_ARMOR;
+				case 1:
+					return SCP_ARMOR;
+			}
+		}
+		return null;
+	} 
 }
